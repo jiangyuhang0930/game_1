5e8ecd6cf47489f31d212a850460f11aea930fdd
jiangyuhang0930 <74084213+jiangyuhang0930@users.noreply.github.com>
2026-07-27 16:45:45 -0700

    Add double jump

--- fighting-game/Scripts/FG/player_controller.gd
+++ fighting-game/Scripts/FG/player_controller.gd
@@ -7,22 +7,30 @@ class_name PlayerController
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
+var jump_charges = 1
 
 var is_attacking = false
 
 func _input(event):
 	# Handle jump.
-	if event.is_action_pressed("jump") and is_on_floor():
+	if event.is_action_pressed("jump") and (is_on_floor() or jump_charges > 0):
+		jump_charges -= 1
 		velocity.y = jump_power * jump_multiplier
 	
 	if event.is_action_pressed("attack"):
 		is_attacking = true
 		sword_slash.visible = true
+		
+	if event.is_action_pressed("dash"):
+		print("dash")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 3
+		
+	if is_on_floor():
+		jump_charges = 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
