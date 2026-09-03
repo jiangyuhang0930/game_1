56da582dabc66efe47f289f2a660ab17febe7aec
Algebra6 <56374447+Algebra6@users.noreply.github.com>
2026-09-03 01:30:09 -0400

    Add turn system and enemy unit support
    Add turn system and Goblin support
    Improve unit movement and selection
    Add End Turn and undo movement
    Fix repeated movement issue

--- fighting-game/Scripts/WC/main.gd
+++ fighting-game/Scripts/WC/main.gd
@@ -4,10 +4,9 @@ extends Node2D
 func _ready():
 	print("Battle Game Start")
 
-
 func _process(_delta):
 	pass
-	
+
 func _input(event):
 	if event.is_action_pressed("switch_fg"):
 		get_tree().change_scene_to_file("res://Scenes/FG/stage_1.tscn")
